55d7329c38865969948e8b02652f833844cfb5f7
phyrum <phyrum@open.org.kh>
2016-06-09 16:59:09 +0700

    login-register forms

--- resources/views/layouts/_navigation.blade.php
+++ resources/views/layouts/_navigation.blade.php
@@ -5,15 +5,16 @@
     </div>
   </form> -->
   <ul class="nav menu">
-    <li class="active"><a href="index.html"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Home </a></li>
+    <li class="active"><a href=""><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Home </a></li>
     <li><a href=""><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg></a></li>
-    <li><a href=""><svg class="glyph stroked line-graph"><use xlink:href="#stroked-line-graph"></use></svg> </a></li>
+    <!-- <li><a href=""><svg class="glyph stroked line-graph"><use xlink:href="#stroked-line-graph"></use></svg> </a></li>
     <li><a href=""><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> </a></li>
     <li><a href=""><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> </a></li>
     <li><a href=""><svg class="glyph stroked app-window"><use xlink:href="#stroked-app-window"></use></svg> </a></li>
-    <li><a href=""><svg class="glyph stroked star"><use xlink:href="#stroked-star"></use></svg> </a></li>
+    <li><a href=""><svg class="glyph stroked star"><use xlink:href="#stroked-star"></use></svg> </a></li> -->
     <li role="presentation" class="divider"></li>
-    <li><a href="login.html"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Login</a></li>
+    <li><a href="login"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Login</a></li>
+    <li><a href="register"><svg class="glyph stroked add-single-female-user"><use xlink:href="#stroked-add-single-female-user"></use></svg> Register</a></li>
   </ul>
 
 </div><!--/.sidebar-->
