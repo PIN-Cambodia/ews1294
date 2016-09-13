78d60280a3987a1f80d9db54e5537154c58f2206
phyrum <phyrum@open.org.kh>
2016-09-13 17:03:30 +0700

    finish insert data into calllogs

--- app/Http/Controllers/ReceivingCallLogAPIController.php
+++ app/Http/Controllers/ReceivingCallLogAPIController.php
@@ -10,24 +10,28 @@ use App\User;
 
 use App\Role;
 use App\Permission;
-use App\Model\Activities;
+use App\Models\Calllogs;
 
 use App\Http\Requests;
 use Response;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ReceivingCallLogAPIController extends Controller
 {
-  public function callLogAPI($json_data)
+  //public function callLogAPI($json_data)
+  public function callLogAPI()
   {
     /* json_decode($json_string, true)
     * When TRUE, returned objects will be converted into associative arrays.
     */
-    $data = json_decode($json_data, true);
-    foreach ($data as $data_key => $data_value)
+    $parsing_json_data = json_decode(Input::get('clog'), true);
+    $call_log = new Calllogs;
+    foreach ($parsing_json_data as $data_key => $data_value)
     {
-      echo "key= " . $data_key . "; value= " . $data_value . "<br />";
+      $call_log -> $data_key = $data_value;
+      $call_log -> save();
     }
 
   }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-          User::create(array(
-              'name'     => 'Chris Sevilleja',
-              'email'    => 'chris@scotch.io',
-              'password' => Hash::make('awesome'),
-              'api_token' => str_random(60),
-          ));
+        $check_existing_user_1 =  User::where('name','=', "Chris Sevilleja")->first();
+        if(!$check_existing_user_1)
+        {
+            User::create(array(
+                'name'     => 'Chris Sevilleja',
+                'email'    => 'chris@scotch.io',
+                'password' => Hash::make('awesome'),
+                'api_token' => str_random(60),
+            ));
+        }
+        // Create authorize api_token user for retrieving callLog from Twilio IVR system
+        $check_existing_user_2 = User::where('name','=', "Twilio user")->first();
+        if(!$check_existing_user_2)
+        {
+            User::create(array(
+                'name'     => 'Twilio user',
+                'email'    => 'twilio@oi.org',
+                'password' => Hash::make('tw_%*'),
+                'api_token' => str_random(60),
+            ));
+        }
+
     }
 }
