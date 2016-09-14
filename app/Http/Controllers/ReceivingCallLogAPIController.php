e8cc28e25db3438c9382efb94644fbcaff50be58
phyrum <phyrum@open.org.kh>
2016-09-14 16:59:28 +0700

    modified callLog API

--- app/Http/Controllers/ReceivingCallLogAPIController.php
+++ app/Http/Controllers/ReceivingCallLogAPIController.php
@@ -27,16 +27,16 @@ class ReceivingCallLogAPIController extends Controller
     */
     $parsing_json_data = json_decode(Input::get('clog'), true);
     $call_log = new Calllogs;
-    $call_log -> activity_id = $parsing_json_data['activity_id'];
-    $call_log -> phone_number = $parsing_json_data['phone_number'];
+    $call_log -> phone_number = $parsing_json_data['phone'];
     $call_log -> result = $parsing_json_data['status'];
     $call_log -> duration = $parsing_json_data['duration'];
+    $call_log -> called_time = $parsing_json_data['date'] . " " . $parsing_json_data['time'] ;
     $call_log -> no_of_retries = $parsing_json_data['retries'];
     $call_log -> project_id = $parsing_json_data['project_id'];
     $call_log -> call_flow_id = $parsing_json_data['call_flow_id'];
-    $call_log -> called_time = $parsing_json_data['called_time'];
     $call_log -> retry_time = $parsing_json_data['retry_time'];
     $call_log -> max_retry = $parsing_json_data['max_retry'];
+    $call_log -> activity_id = $parsing_json_data['activity_id'];
     $success = $call_log -> save();
     if(!empty($success)){ return "Success: " . Input::get('clog');}
     else { return "Error: Data cannot be inserted.";}
