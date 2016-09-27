f7bafeace7b0c3e5c69885151d49aa932372250f
Samak <csamak@open.org.kh>
2016-09-27 18:35:13 +0700

    add

--- public/js/ajax-district.js
+++ public/js/ajax-district.js
@@ -150,8 +150,8 @@ $('form#uploadForm').on('submit',function(event){
                  success: function(activityId)
                  {
                     formData.append('api_token','C5hMvKeegj3l4vDhdLpgLChTucL9Xgl8tvtpKEjSdgfP433aNft0kbYlt77h');
-                    // formData.append('contacts',phones);
-                     formData.append('contacts', '[{"phone":"017696365"},{"phone":"012415734"}]');
+                    formData.append('contacts',phones);
+                    // formData.append('contacts', '[{"phone":"017696365"},{"phone":"012415734"}]');
 
                     formData.append('activity_id',activityId);
                     formData.append('no_of_retry',3);
