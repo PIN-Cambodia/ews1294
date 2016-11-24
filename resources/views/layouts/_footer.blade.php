9d142473af315338e978f0df47b9afa4648ad766
phyrum <phyrum@open.org.kh>
2016-11-24 14:57:47 +0700

    change footer layout

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,25 +1,24 @@
 <div id="footer">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-8 col-md-8 col-lg-8 "></div>
-			<div class="col-xs-4 col-md-4 col-lg-4 ">
-				<div class="col-xs-5 col-md-5 col-lg-5"></div>
-				<div class="col-xs-3 col-md-3 col-lg-3 text-right">
-					<a href="http://ec.europa.eu/echo/what/humanitarian-aid/risk-reduction_en" target="_blank">
-						<img src="EU.png" height="50px">
-					</a>
-				</div>
-				<div class="col-xs-2 col-md-2 col-lg-2 text-right">
-					<a href="http://www.ncdm.gov.kh/" target="_blank">
-						<img src="NCDM.png" height="50px">
-					</a>
-				</div>
-				<div class="col-xs-2 col-md-2 col-lg-2 text-right">
-					<a href="https://www.facebook.com/PINCambodiacz/" target="_blank">
-						<img src="PIN.png" height="50px">
-					</a>
-				</div>
+	<div class="row">
+		<div class="col-xs-7 col-md-8 col-lg-9 "></div>
+		<div class="col-xs-5 col-md-4 col-lg-3 ">
+			<div class="col-lg-4"></div>
+			<div class="col-lg-3 text-center">
+				<a href="http://ec.europa.eu/echo/what/humanitarian-aid/risk-reduction_en" target="_blank">
+					<img src="EU.png" height="50px">
+				</a>
 			</div>
+			<div class="col-lg-2 text-center">
+				<a href="http://www.ncdm.gov.kh/" target="_blank">
+					<img src="NCDM.png" height="50px">
+				</a>
+			</div>
+			<div class="col-lg-2 text-center">
+				<a href="https://www.facebook.com/PINCambodiacz/" target="_blank">
+					<img src="PIN.png" height="50px">
+				</a>
+			</div>
+			<div class="col-lg-1"></div>
 		</div>
 	</div>
 </div>
