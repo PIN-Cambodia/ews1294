2587f19c3e2f6f1fab35189d8407a985c810c87e
phyrum <phyrum@open.org.kh>
2016-11-24 11:18:15 +0700

    modified ReceivingCallLogAPIController for inserting data of call log status as integer

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -176,7 +176,7 @@ Route::group(['prefix' => 'api/v1', 'middleware' => 'auth:api'], function()
 });
 
 // CallLog report
-Route::get('/calllogreport', ['middleware' => 'auth', 'uses' => 'CallLogReportController@CallLogReportView']);
+Route::get('/calllogreport', ['middleware' => 'auth', 'uses' => 'CallLogReportController@CallLogReportView'])->middleware('auth');
 Route::post('/getCallLogReport', ['middleware' => 'auth', 'uses' => 'CallLogReportController@getCallLogReport']);
 
 
