6a8fc93cd72a728625ac4b081ed813c0ead0788e
Samak <csamak@open.org.kh>
2016-12-02 18:42:20 +0700

    fix map

--- resources/views/sensorsMap.blade.php
+++ resources/views/sensorsMap.blade.php
@@ -59,15 +59,13 @@
     );
     vectorLayer.addFeatures(feature);
 
-  var marker = new OpenLayers.Marker(101.2336,13.3665);
-  marker.id = " {{$sensor->id}} ";
-          @endforeach
+  @endforeach
   /*var marker = new OpenLayers.Marker(101.2336,13.3665);
-  marker.id = " {{$sensor->id}} ";*/
+  marker.id = "{{$sensor->id}} ";
   marker.events.register("click",marker,function () {
     //alert('test on click on map');
   });
-
+   */
 //  var feature = new OpenLayers.Feature.Vector(
 //          new OpenLayers.Geometry.Point( 105.96,12.55  ).transform(epsg4326, projectTo),
 //          {description:'Big Ben'} ,
