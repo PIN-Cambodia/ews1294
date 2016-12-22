14a19773b98132f09bfd420a9356b629bcd7ba74
phyrum <phyrum@open.org.kh>
2016-12-22 14:26:51 +0700

    add AWS php.ini on upload_max_filesize and post_max_size in .ebextensions/

--- resources/views/emails/sensoremail.blade.php
+++ resources/views/emails/sensoremail.blade.php
@@ -1,24 +1,26 @@
-<div class="col-sm-6 col-md-6 col-lg-6 main">
-    <div class="row">
-        <div class="col-sm-12 col-md12 col-lg-12">
-            <h3> THIS IS AN AUTOMATICALLY GENERATED MESSAGE. </h3>
-            <h3> To the members of the Early Warning System Alert Group, </h3>
-            <p> {!! $content !!} </p>
-            <p> As a member of the Early Warning System team, we ask that you carry out any necessary actions to respond to these flood levels as well. </p>
-            <br /> Regards,<br />
-            The EWS1294 Team <br />
-            An NCDM Initiative
-        </div>
-    </div><br />
-    <div class="row">
-        <div class="col-sm-12 col-md12 col-lg-12">
-            <h3> ផ្ញើសារដោយស្វ័យប្រវត្តិ </h3>
-            <h3> ជូន​ចំពោះ​សមាជិក​នៃ​ប្រព័ន្ធ​ប្រកាស​ឲ្យ​ដឹង​មុន​, </h3>
-            <p> {!! $content_kh !!} </p>
-            <p> ក្នុង​នាម​ជា​សមាជិក​នៃ​ប្រព័ន្ធ​ប្រកាស​ឲ្យ​ដឹង​មុន ​យើង​សុំ​ឱ្យ​អ្នក​អនុវត្ត​សកម្មភាព​ដែល​ចាំបាច់​ដើម្បី​ឆ្លើយ​តប​ទៅ​នឹង​កម្រិត​ទឹក​ជំនន់​ទាំង​នេះ​ផង​ដែរ​។ </p>
-            <br /> ដោយ​សេចក្ដី​គោរព​ពី​, <br />
-            ក្រុម EWS1294 <br />
-            គម្រោង​ផ្តួចផ្តើម​របស់​គណៈកម្មាធិការ​ជាតិ​គ្រប់គ្រង​គ្រោះ​មហន្តរាយ
+<font face="arial">
+    <div class="col-sm-6 col-md-6 col-lg-6 main">
+        <div class="row">
+            <div class="col-sm-12 col-md12 col-lg-12">
+                <h3> THIS IS AN AUTOMATICALLY GENERATED MESSAGE. </h3>
+                <h3> To the members of the Early Warning System Alert Group, </h3>
+                <p> {!! $content !!} </p>
+                <p> As a member of the Early Warning System team, we ask that you carry out any necessary actions to respond to these flood levels as well. </p>
+                <br /> Regards,<br />
+                The EWS1294 Team <br />
+                An NCDM Initiative
+            </div>
+        </div><br />
+        <div class="row">
+            <div class="col-sm-12 col-md12 col-lg-12">
+                <h3> ផ្ញើសារដោយស្វ័យប្រវត្តិ </h3>
+                <h3> ជូន​ចំពោះ​សមាជិក​នៃ​ប្រព័ន្ធ​ប្រកាស​ឲ្យ​ដឹង​មុន​, </h3>
+                <p> {!! $content_kh !!} </p>
+                <p> ក្នុង​នាម​ជា​សមាជិក​នៃ​ប្រព័ន្ធ​ប្រកាស​ឲ្យ​ដឹង​មុន ​យើង​សុំ​ឱ្យ​អ្នក​អនុវត្ត​សកម្មភាព​ដែល​ចាំបាច់​ដើម្បី​ឆ្លើយ​តប​ទៅ​នឹង​កម្រិត​ទឹក​ជំនន់​ទាំង​នេះ​ផង​ដែរ​។ </p>
+                <br /> ដោយ​សេចក្ដី​គោរព​ពី​, <br />
+                ក្រុម EWS1294 <br />
+                គម្រោង​ផ្តួចផ្តើម​របស់​គណៈកម្មាធិការ​ជាតិ​គ្រប់គ្រង​គ្រោះ​មហន្តរាយ
+            </div>
         </div>
     </div>
-</div>
+</font>
