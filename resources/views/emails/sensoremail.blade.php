55c37193f6fe18b76ba3e096b7c40ee46d3a9369
phyrum <phyrum@open.org.kh>
2016-12-22 10:49:48 +0700

    modified sensoremail view

--- resources/views/emails/sensoremail.blade.php
+++ resources/views/emails/sensoremail.blade.php
@@ -3,7 +3,7 @@
         <div class="col-sm-12 col-md12 col-lg-12">
             <h3> THIS IS AN AUTOMATICALLY GENERATED MESSAGE. </h3>
             <h3> To the members of the Early Warning System Alert Group, </h3>
-            <p> {!! $content !!} </p><br />
+            <p> {!! $content !!} </p>
             <p> As a member of the Early Warning System team, we ask that you carry out any necessary actions to respond to these flood levels as well. </p>
             <br /> Regards,<br />
             <b> The EWS1294 Team </b><br />
@@ -14,7 +14,7 @@
         <div class="col-sm-12 col-md12 col-lg-12">
             <h3> ផ្ញើសារដែលដោយស្វ័យប្រវត្តិ </h3>
             <h3> ជូន​ចំពោះ​សមាជិក​នៃ​ប្រព័ន្ធ​ប្រកាស​ឲ្យ​ដឹង​មុន​, </h3>
-            <p> {!! $content_kh !!} </p><br />
+            <p> {!! $content_kh !!} </p>
             <p> ក្នុង​នាម​ជា​សមាជិក​នៃ​ប្រព័ន្ធ​ប្រកាស​ឲ្យ​ដឹង​មុន ​យើង​សុំ​ឱ្យ​អ្នក​អនុវត្ត​សកម្មភាព​ដែល​ចាំបាច់​ដើម្បី​ឆ្លើយ​តប​ទៅ​នឹង​កម្រិត​ទឹក​ជំនន់​ទាំង​នេះ​ផង​ដែរ​។ </p>
             <br /> ដោយ​សេចក្ដី​គោរព​ពី​, <br />
             <b> ក្រុម EWS1294 </b><br />
