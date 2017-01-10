c10263b3c8e64fcaaf1ff51bea2f2fba3323c85f
phyrum <phyrum@open.org.kh>
2017-01-10 16:04:47 +0700

    add timeout in ajax-district.js

--- public/js/ajax-district.js
+++ public/js/ajax-district.js
@@ -295,7 +295,7 @@ $(document).ready(function(){
                         $.ajax({
                             url: 'http://ews-twilio.ap-southeast-1.elasticbeanstalk.com/api/v1/processDataUpload',
                             method: 'POST',
-                            timeout: 20000,
+                            timeout: 600000,
                             data: formDataTwillioAPI,
                             success: function(data) {
                                 $('#modal_waiting').modal('hide');
