a7272b9680a2559fbe47fe428481e6a573a52079
Chenda <chenda>
2017-02-06 23:14:00 -0500

    test change langauge with text header

--- resources/lang/km/menus.php
+++ resources/lang/km/menus.php
@@ -19,7 +19,7 @@ return [
     'get_phones_from_call_log' => 'លេខ​ទូរសព្ទ​​ពីការ​ហៅ​ទូរសព្ទ',
     'get_phones_from_call_log_registration' => 'លេខ​ទូរសព្ទពីការចុះឈ្មោះក្នុងការ​ហៅ​ទូរសព្ទ',
     'api_wiki' => 'API វីគី',
-    // 'ews_header_str' => 'Early Warning System',
+    'ews_header_str' => 'Early Warning System',
     'ews_header_str2' => 'ប្រព័ន្ធប្រកាសឲ្យដឹងមុន',
     'sensors_mgt' => 'ការ​គ្រប់គ្រង​ឧបករណ៌​ចាប់​សញ្ញា',
     'sensors_map' => 'ផែនទី​ឧបករណ៌​ចាប់​សញ្ញា',
