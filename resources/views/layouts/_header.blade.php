a3fe83a782ff5fa190ebc8f7fdd9492f4db50c80
Chenda <chenda>
2017-02-06 21:49:43 -0500

    change coding in header page

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,10 +9,10 @@
 
           <div class="col-sm-9 col-md-9 col-lg-9">
               <h4 style="text-align: center; margin-left: 10px; font-size: 15px">
-              <b>{{trans(menus.ews_header_str)}}</b>             
+              <b>{{trans('menus.ews_header_str')}}</b>             
                </h4>
               <h4 style="margin-left: 126px; margin-top: -60px; position: absolute; font-size: 17px; text-align: center;" >
-              <b>{{trans(menu.ews_header_str2)}}</b>
+              <b>{{trans('menu.ews_header_str2')}}</b>
               </h4>             
 
           </div>
