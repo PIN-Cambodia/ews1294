bdf839d6fcc9c7194bc369d66c40babfcc2af39d
Chenda <chenda>
2017-02-16 23:16:55 -0500

    test

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -37,7 +37,7 @@ class sensorLogChartCtrl extends Controller
         {
             // retrieve first 24 readings for $sensor_id
             $sensorlogs = DB::table('sensorlogs')
-                ->select(DB::raw("id, date_format('H:i:s') as time, stream_height"))
+                ->select(DB::raw("id, date_format(timestamp,'H:i:s') as time, stream_height"))
                 ->where('sensor_id','=',$sensor_id)->orderBy('time')->limit(24)->get();
         }
         // select sensortrigger info from database
