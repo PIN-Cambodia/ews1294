e5f2b94c4377914f9359b31a397ec589d50bfb0a
Chenda <chenda>
2017-02-22 21:33:55 -0500

    add scroll

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -12,7 +12,7 @@
         <div class="panel panel-default">
           <div class="panel-heading" ><center><b>{{ trans('menus.about_us') }} </b></center> </div>
             <br />
-           <div class="infinitescroll" id="myInfiniteScroll1" style="height: 500px; overflow: scroll;">
+           <div class="infinitescroll" id="myInfiniteScroll1" style="height: 150px; overflow: scroll;">
          
             <div class="row"> 
                 <div class="col-md-2 col-lg-2"></div>
