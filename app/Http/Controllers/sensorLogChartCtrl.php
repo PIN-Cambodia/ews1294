0a6b00ec6a1a03991ed07c1fa32210aa66719897
Chenda <chenda>
2017-03-05 20:42:02 -0500

    change from time to timestamp

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -37,7 +37,7 @@ class sensorLogChartCtrl extends Controller
         {
             // retrieve first 24 readings for $sensor_id
             $sensorlogs = DB::table('sensorlogs')
-                ->select(DB::raw("id, timestamp, stream_height"))
+                ->select(DB::raw("id, date_format(timestamp,'%H:%i') as time, stream_height"))
                 ->where('sensor_id','=',$sensor_id)->orderBy('timestamp')->limit(24)->get();
         }
         // select sensortrigger info from database
@@ -51,7 +51,7 @@ class sensorLogChartCtrl extends Controller
             // add row data into datatable for Chart
             foreach($sensorlogs as $v => $sensorlog)
             {
-                $sensenlogTable->addRow([$sensorlog->time , $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
+                $sensenlogTable->addRow([$sensorlog->timestamp , $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
             }
             // generate Chart as a LineChart
             Lava::LineChart('SensorLogChart',$sensenlogTable)
