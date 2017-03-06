5c88d5d5af885dce16abe3d0ea1c42f9d1126976
Chenda <chenda>
2017-03-05 23:36:04 -0500

    put DESC

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -39,7 +39,7 @@ class sensorLogChartCtrl extends Controller
             // date_format(timestamp,'%H:%i') as time
             $sensorlogs = DB::table('sensorlogs')
                 ->select(DB::raw("id, timestamp, stream_height"))
-                ->where('sensor_id','=',$sensor_id)->orderBy('timestamp DESC')->limit(24)->get();
+                ->where('sensor_id','=',$sensor_id)->orderBy('timestamp')->limit(24)->get();
         }
         // select sensortrigger info from database
         $sensortrigger = DB::table('sensortriggers')
