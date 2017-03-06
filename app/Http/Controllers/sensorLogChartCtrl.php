bb8c383a5226cb722c2ac75b9a78af084444923d
Chenda <chenda>
2017-03-06 00:06:20 -0500

    git desc

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -28,7 +28,7 @@ class sensorLogChartCtrl extends Controller
         {
             // retrieve first reading within last 30 days for $sensor_id
             $sensorlogs = DB::table('sensorlogs')
-                ->select(DB::raw("id, date_format(date(date_sub(timestamp,interval 0 hour)),DATE() as time, stream_height"))
+                ->select(DB::raw("id, date_format(date(date_sub(timestamp,interval 0 hour)),GET_FORMAT(DATE,'ISO')) as time, stream_height"))
                 ->where('sensor_id','=',$sensor_id)
                 ->groupBy('time')
                 ->orderBy('timestamp')->limit(30)->get();
@@ -36,14 +36,9 @@ class sensorLogChartCtrl extends Controller
         else
         {
             // retrieve first 24 readings for $sensor_id
-            // date_format(timestamp,'%H:%i') as time
             $sensorlogs = DB::table('sensorlogs')
                 ->select(DB::raw("id, timestamp, stream_height"))
-<<<<<<< HEAD
-                ->where('sensor_id','=',$sensor_id)->orderBy('timestamp','asc')->limit(24)->get();
-=======
-                ->where('sensor_id','=',$sensor_id)->orderBy('timestamp')->limit(24)->get();
->>>>>>> parent of f1d301c... add desc
+                ->where('sensor_id','=',$sensor_id)->orderBy('timestamp','desc')->limit(24)->get();
         }
         // select sensortrigger info from database
         $sensortrigger = DB::table('sensortriggers')
