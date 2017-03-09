eb88b52bea22765feb1dc2d86000f70157bee472
Chenda <chenda>
2017-03-09 07:24:06 -0500

    updo coding

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -40,7 +40,7 @@ class sensorLogChartCtrl extends Controller
             $sensorlogs = DB::table('sensorlogs')
                 ->select (DB::raw("id, timestamp as time,  stream_height"))
                 ->where('sensor_id','=',$sensor_id)
-                ->orderBy('timestamp')
+                ->orderBy('timestamp','desc')
                 ->limit(24)->get();
      
 
@@ -52,28 +52,16 @@ class sensorLogChartCtrl extends Controller
             ->where('sensor_id','=',$sensor_id)
             ->first();
 
-        // if(!empty($sensortrigger))
-        // {
-        //     // add row data into datatable for Chart
-        //     if ($graph_type==1) {
+        if(!empty($sensortrigger))
+        {
+           
               
             
-        //     for($i= count($sensorlogs)-1; $i >=0; $i--;)
-        //     {
-        //         $sensenlogTable->addRow([$sensorlog->time, $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
-        //     }
-        //     }
-
-   if(!empty($sensortrigger))
-        {
-            // add row data into datatable for Chart
-            //   foreach($sensorlogs as $v => $sensorlog)
-            if($graph_type==1) {
-                for ($i = count($sensorlogs) - 1; $i >= 0; $i--) {
-                    $sensorlog = $sensorlogs[$i];
-                    $sensenlogTable->addRow([$sensorlog->time, $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
-                }
+            foreach($sensorlogs as $v => $sensorlog)
+            {
+                $sensenlogTable->addRow([$sensorlog->time, $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
             }
+            
             // generate Chart as a LineChart
             Lava::LineChart('SensorLogChart',$sensenlogTable)
                 ->setOptions(['pointSize' => 1,
