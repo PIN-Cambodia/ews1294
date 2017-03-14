bf49677cee86365d81bcc036644d4faf8a77f12c
Chenda <chenda>
2017-03-13 22:22:54 -0400

    add cm

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -67,7 +67,7 @@ class sensorLogChartCtrl extends Controller
             if($graph_type==1) {
                 for ($i = count($sensorlogs) - 1; $i >= 0; $i--) {
                     $sensorlog = $sensorlogs[$i];
-                    $sensenlogTable->addRow([$sensorlog->time, $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
+                    $sensenlogTable->addRow([$sensorlog->time, $sensorlog->stream_height."cm", $sensortrigger->level_emergency, $sensortrigger->level_warning]);
                 }
             }else{
                 foreach($sensorlogs as $v => $sensorlog)
