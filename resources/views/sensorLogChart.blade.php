8274db108c3f79d92e35a9312ba24fbcd5b7be43
Chenda <chenda>
2017-03-14 05:05:36 -0400

    Revert "add new sensor"
    This reverts commit 91f69127ee61d3f4beeaec8f5c3121e2f8ab0bf8.

--- resources/views/sensorLogChart.blade.php
+++ resources/views/sensorLogChart.blade.php
@@ -17,10 +17,10 @@
                             <div class="col-xs-8 col-md-9 col-lg-10 ">
                                 <b>
                                     @if($graph_type=='1')
+
                                         {{ trans('sensors.sensorlog6_graph')}}
                                         {{ date('( d-M-Y )')}}
                                         
-
                                     @else
                                         {{ trans('sensors.sensorlog1threadingOf30days_graph') }}
                                           
