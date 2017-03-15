ff2c4a4a2a64ee4360a1b6beadc8467271c8c46e
Chenda <chenda>
2017-03-15 03:35:51 -0400

    change paramater

--- resources/views/sensorLogChart.blade.php
+++ resources/views/sensorLogChart.blade.php
@@ -17,17 +17,19 @@
                             <div class="col-xs-8 col-md-9 col-lg-10 ">
                                 <b>
                                     @foreach($sensorsChart as $sensorChart)
+                                     {{$sensorChart['sensor_id']}}
+                                    @endforeach
                                     @if($graph_type=='1')
 
                                         {{ trans('sensors.sensorlog6_graph')}}
                                         {{ date('( d-M-Y )')}}
-                                        {{$sensorChart['sensor_id']}}
+                                       
                                      
                                     @else
                                         {{ trans('sensors.sensorlog1threadingOf30days_graph') }}
                                           
                                     @endif
-                                    @endforeach
+                                    
                              
 
                                 </b>
