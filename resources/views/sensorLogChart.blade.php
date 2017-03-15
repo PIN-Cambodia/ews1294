b79c1adb76864cf3da5ac14a11de0c6f31c23523
Chenda <chenda>
2017-03-14 22:44:35 -0400

    remove foreach

--- resources/views/sensorLogChart.blade.php
+++ resources/views/sensorLogChart.blade.php
@@ -17,12 +17,22 @@
                             <div class="col-xs-8 col-md-9 col-lg-10 ">
                                 <b>
 
-                          
+                                
+                               <?php
+                                    $location= "";
+                                    $location_name = \DB::table('commune')->where('CCode', $sensor->location_code)->first();
+                                    if(!empty($location_name)){
+                                        if (\App::getLocale()=='km')
+                                            $location = $location_name->CName_kh;
+                                        else $location = $location_name->CName_en;
+                                    }
+                                ?> 
                                     @if($graph_type=='1')
 
                                         {{ trans('sensors.sensorlog6_graph')}}
-                                        {{ $sensor->additional_location_info }} 
-
+                                        {{ date('( d-M-Y )')}}
+                                  {{ $sensor->additional_location_info }}
+                                        
                                     @else
                                         {{ trans('sensors.sensorlog1threadingOf30days_graph') }}
                                           
