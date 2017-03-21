ee66a5add3036fd041d3c24bd05a94f8d12c1e41
Chenda <chenda>
2017-03-20 23:28:41 -0400

    comment coding content

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -4,20 +4,20 @@
   <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
       <ol class="breadcrumb">
-         <i class="pe-7s-users pe-lg"></i> {{ trans('menus.about_us') }}
+         <i class="pe-7s-users pe-lg"></i> {{ trans('menus.contact_us') }}
       </ol>
     </div><!--/.row-->
     <div class="row">
       <div class="col-xs-12 col-md-12 col-lg-12">
         <div class="panel panel-default">
-          <div class="panel-heading" ><center><b>{{ trans('menus.about_us') }} </b></center> </div>
+          <div class="panel-heading" ><center><b>{{ trans('menus.contact_us') }} </b></center> </div>
             <br />
            <div style="height:670px; overflow-y: scroll; overflow-x: hidden;">
             <div class="row"> 
                 <div class="col-md-2 col-lg-2"></div>
                 <div class="col-md-8 col-lg-8">
                 	<!-- {!! Form::open(array('route' => 'contact_store', 'class' => 'form')) !!} -->
-                	{!! Form::open() !!}
+                <!-- 	{!! Form::open() !!}
 
 						<div class="form-group">
 						    {!! Form::label('Your Name') !!}
@@ -48,11 +48,8 @@
 						      array('class'=>'btn btn-primary')) !!}
 						</div>
 					{!! Form::close() !!}
-
-
-
-
-                </div><!-- \ panel panel-body -->
+ -->
+   </div><!-- \ panel panel-body -->
         </div><!-- \ panel panel-default -->
       </div>
     </div><!--/.row-->
