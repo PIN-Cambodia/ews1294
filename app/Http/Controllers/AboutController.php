4eefd1a4949aa448589ac6f698ea2114b5584ad5
Chenda <chenda>
2017-03-21 05:35:58 -0400

    change update function send mai

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -1,5 +1,6 @@
 <?php
  namespace App\Http\Controllers;
+ use Illuminate\Http\Request\ContactFormRequest;
 
 class AboutController extends Controller {
 
@@ -11,6 +12,18 @@ class AboutController extends Controller {
     
   public function store(ContactFormRequest $request)
     {
+
+
+    \Mail::send('emails.contact',
+        array(
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'user_message' => $request->get('message')
+        ), function($message)
+    {
+        $message->from('wj@wjgilmore.com');
+        $message->to('chenda.loeurt@gamil.com', 'Admin')->subject('Feedback');
+    });
     	return \Redirect::route('contact')
       ->with('message', 'Thanks for contacting us!');
     }
