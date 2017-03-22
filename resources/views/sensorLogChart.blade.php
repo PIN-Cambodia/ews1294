b966fdaadd1d7617cb8b07538313e5d1dfb0568d
Chenda <chenda>
2017-03-22 03:24:58 -0400

    change style

--- resources/views/sensorLogChart.blade.php
+++ resources/views/sensorLogChart.blade.php
@@ -28,14 +28,13 @@
                                         {{ trans('sensors.sensorlog1threadingOf30days_graph') }}
                                           
                                     @endif
-                                     </br>
+                                    
 
                                    <!-- add update next time with database (show location header) -->
                                    
                                     @if($sensorId =='1020301')
                                     {{ trans('sensors.kompot') }}
-                                    <br>
-
+                                    
                                     @else
 
                                     {{ trans('sensors.phnom_penh') }}
