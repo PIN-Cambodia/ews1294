83f18572f4c737cffe0543ec890dc1e4fc1501a9
Chenda <chenda>
2017-03-23 02:27:14 -0400

    set default coding

--- config/mail.php
+++ config/mail.php
@@ -30,7 +30,7 @@ return [
     */
 
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-    'host' =>env('MAIL_HOST','smtp.gmail.com'),
+    // 'host' =>env('MAIL_HOST','smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -59,10 +59,10 @@ return [
         'address' => 'alerts@ews1294.info',
         'name' => 'Early Warning System',
     ],
-     'from' => [
-        'address' => 'vcgroup3laravel@gmail.com',
-        'name' => 'test',
-    ],
+    //  'from' => [
+    //     'address' => 'vcgroup3laravel@gmail.com',
+    //     'name' => 'test',
+    // ],
 
 
     /*
@@ -90,7 +90,7 @@ return [
     */
 
     'username' => env('MAIL_USERNAME'),
-    'username'=> env('vcgroup3laravel@gmail.com'),
+    // 'username'=> env('vcgroup3laravel@gmail.com'),
 
 
     /*
@@ -105,7 +105,7 @@ return [
     */
 
     'password' => env('MAIL_PASSWORD'),
-    'password' => env('administratorlaravelgroup3'),
+    // 'password' => env('administratorlaravelgroup3'),
 
 
     /*
