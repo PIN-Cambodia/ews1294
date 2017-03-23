231085082b070e6836f9c3037d580d2109606d5f
Chenda <chenda>
2017-03-23 03:42:10 -0400

    update coding

--- config/mail.php
+++ config/mail.php
@@ -90,7 +90,7 @@ return [
     */
 
     'username' => env('MAIL_USERNAME'),
-    // 'username'=> env('vcgroup3laravel@gmail.com'),
+    'username'=> env('vcgroup3laravel@gmail.com'),
 
 
     /*
@@ -105,7 +105,7 @@ return [
     */
 
     'password' => env('MAIL_PASSWORD'),
-    // 'password' => env('administratorlaravelgroup3'),
+   'password' => env('administratorlaravelgroup3'),
 
 
     /*
