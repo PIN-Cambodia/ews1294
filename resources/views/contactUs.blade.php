874d1c357e5f4f51b1556abf8daa590bf490cebf
Chenda <chenda>
2017-03-22 22:09:28 -0400

    update

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -22,7 +22,7 @@
 				    @endforeach -->
 				</ul>
 
-				{!! Form::open(array(class' => 'form' )) !!}
+				{!! Form::open(array('class' => 'form' )) !!}
 
 				<div class="form-group">
 				    {!! Form::label('Your Name') !!}
