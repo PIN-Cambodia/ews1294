5f528dc4485449e7487a30969ec01c9114286c76
Chenda <chenda>
2017-03-24 03:36:12 -0400

    add coding with return result

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,29 +15,47 @@ Class ContactController extends Controller
 
     }
 
-    Public function postContact(Request $request){
-        $this->validate($request,[
-            'email'=> 'required|email',
-            'name' => 'required',
-            'message' => 'required']);
-        $data = array(
-            'email'=> $request->email,
-            'name'=> $request->name,
-            'message'=> $request->message
+//     Public function postContact(Request $request){
+//         $this->validate($request,[
+//             'email'=> 'required|email',
+//             'name' => 'required',
+//             'message' => 'required']);
+//         $data = array(
+//             'email'=> $request->email,
+//             'name'=> $request->name,
+//             'message'=> $request->message
 
-            );
+//             );
 
-     Mail::send('contactUs', $data, function($messages) use ($data){
-        $messages->from($data['email']);
-        $messages->to('chenda.loeurt@gmail.com');
-        $messages->subject($data['message']);
+//      Mail::send('contactUs', $data, function($messages) use ($data){
+//         $messages->from($data['email']);
+//         $messages->to('vcgroup3laravel@gmail.com');
+//         $messages->subject($data['message']);
 
-     });
-   // Session::flash('success','Your Email was Sent!');
-    return redirect('contactUs')
-    ->with('message','you have successful contact us');
+//      });
+//    // Session::flash('success','Your Email was Sent!');
+//     return redirect('contactUs')
+//     ->with('message','you have successful contact us');
 
 
+
+// }
+
+public function postContact(Request $request)
+{
+
+    Mail::send('emails.contactUs',
+        array(
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'user_message' => $request->get('message')
+        ), function($message)
+    {
+        $message->from('wj@wjgilmore.com');
+        $message->to('vcgroup3laravel@gmail.com', 'Admin')->subject('TODOParrot Feedback');
+    });
+
+  return \Redirect::route('contactUs')->with('message', 'Thanks for contacting us!');
 
 }
  
