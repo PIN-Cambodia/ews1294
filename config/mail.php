3be8ecd17fa796406c546c3ecaba8a1f5659bdf6
Chenda <chenda>
2017-03-23 23:15:51 -0400

    add coding with return result

--- config/mail.php
+++ config/mail.php
@@ -61,7 +61,7 @@ return [
     ],
      'from' => [
         'address' => 'vcgroup3laravel@gmail.com',
-        'name' => '',
+        'name' => 'ews',
     ],
     
     
@@ -91,7 +91,7 @@ return [
     */
 
     'username' => env('MAIL_USERNAME'),
-    //'username'=> env('vcgroup3laravel@gmail.com'),
+   'username'=> env('vcgroup3laravel@gmail.com'),
 
 
     /*
@@ -106,7 +106,7 @@ return [
     */
 
     'password' => env('MAIL_PASSWORD'),
-    //'password' => env('administratorlaravelgroup3'),
+    'password' => env('administratorlaravelgroup3'),
 
 
     /*
