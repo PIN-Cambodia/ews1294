e13c721b39fbdd857a4091f9502845297537fc19
Chenda <chenda>
2017-03-26 13:43:38 -0400

    update send email

--- app/Http/Controllers/Mailer/Sendemail.php
+++ app/Http/Controllers/Mailer/Sendemail.php
@@ -4,11 +4,12 @@ require 'phpmailer/PHPMailerAutoload.php';
 function Sendemail($fromname, $to, $namereciever, $subject, $body)
 {
 
-	$mail = new PHPMailer;
+	$mail = new PHPMailer(true);
 	//Set PHPMailer to use SMTP.
+		//Enable SMTP debugging. 
+	$mail->SMTPDebug = 2;  // SET to 3 to see errors
 	$mail->isSMTP(); 
-	//Enable SMTP debugging. 
-	$mail->SMTPDebug = 2;  // SET to 3 to see errors                              
+                              
 	           
 	//Set SMTP host name  
 	$mail->Mailer = "smtp";                        
@@ -16,8 +17,8 @@ function Sendemail($fromname, $to, $namereciever, $subject, $body)
 	//Set this to true if SMTP host requires authentication to send email
 	$mail->SMTPAuth = true;                          
 	//Provide username and password - DO NOT CHANGE IT -    
-	$mail->Username = "vcgroup3laravel@gmail.com";                 
-	$mail->Password = "administratorlaravelgroup3";                           
+	$mail->Username = "chenda.loeurt@gmail.com";                 
+	$mail->Password = "chendapnc";                           
 	//If SMTP requires TLS encryption then set it
 	$mail->SMTPSecure = "tls";                           
 	//Set TCP port to connect to 
@@ -30,7 +31,7 @@ function Sendemail($fromname, $to, $namereciever, $subject, $body)
 	// MAIL THAT YOU WANT TO SEND
 	$mail->FromName = $fromname;
 	$mail->addAddress($to, $namereciever); // Define address of destination
-	$mail->isHTML(true);
+	$mail->isHTML(false);
 
 	// DEFINE THE MAIl CONTENT
 	$mail->Subject = $subject;
