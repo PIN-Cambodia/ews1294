5ec4d9a6c51d5e485e398e02d308a22428b0a591
Chenda <chenda>
2017-03-30 00:07:27 -0400

    successful send email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -41,7 +41,7 @@ Class ContactController extends Controller
 
       
         $org =$data['email'];
-        $email = "chenda.loeurt@peopleinneed.cz";
+        $email = "chenda.loeurt@gmail.com";
         $title = "Contact from EWS";
         $body = $data['user_message'];
         $fname = $data['name'];
