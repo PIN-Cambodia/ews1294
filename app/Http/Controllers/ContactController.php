5c359514af32b3591565ececa4ef3af720da5ce7
Chenda <chenda>
2017-03-31 00:14:23 -0400

    test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -41,7 +41,7 @@ Class ContactController extends Controller
 
    
         $org =$data['email'];
-        $email = "chenda.loeurt@peopleinneed.cz.ost";
+        $email = "james.happell@peopleinneed.cz";
         $title = "Contact from EWS";
         $body = $data['user_message'];
         $fname = $data['name'];
