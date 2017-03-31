1ebca98c19cc6cf6df086c52750cb668e07612fc
Chenda <chenda>
2017-03-30 21:51:36 -0400

    fix error sending email to local email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -41,7 +41,7 @@ Class ContactController extends Controller
 
    
         $org =$data['email'];
-        $email = "vannchhai.pov@gmail.com";
+        $email = "loeche01@pinf.cz";
         $title = "Contact from EWS";
         $body = $data['user_message'];
         $fname = $data['name'];
