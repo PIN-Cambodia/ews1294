b43195a06c1a2e74685ee1bad636d96a2f03a388
Chenda <chenda>
2017-03-30 22:50:39 -0400

    test

--- app/Http/Controllers/Mailer/Sendemail.php
+++ app/Http/Controllers/Mailer/Sendemail.php
@@ -45,7 +45,8 @@ function Sendemail($fromname, $to, $namereciever, $subject, $body)
 	else 
 	{
 		// \Session::flash('message_required_activate','Result has been Sent!');
-	 \Session::flash('message_success_send','Result has been Sent!');
+	 // \Session::flash('message_success_send','Result has been Sent!');
+		echo "successful";
 	
 	}
 }
