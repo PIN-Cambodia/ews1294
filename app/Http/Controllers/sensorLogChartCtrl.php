95e66a40f950de3494ee215fc980c88b5de2e030
chenda <chenda.loeurt@gmail.com>
2017-06-08 16:06:35 +0700

    Update sensor chart

--- app/Http/Controllers/sensorLogChartCtrl.php
+++ app/Http/Controllers/sensorLogChartCtrl.php
@@ -33,7 +33,7 @@ class sensorLogChartCtrl extends Controller
                 ->select(DB::raw("id, date_format(date(date_sub(timestamp,interval 0 hour)),GET_FORMAT(DATE,'ISO')) as time, stream_height"))
                 ->where('sensor_id','=',$sensor_id)
                 ->groupBy('time')
-                ->orderBy('timestamp')
+                ->orderBy('id','desc')
                 ->limit(30)->get();
         }
         else
@@ -42,9 +42,7 @@ class sensorLogChartCtrl extends Controller
             $sensorlogs = DB::table('sensorlogs')
                 ->select (DB::raw("id,date_format(timestamp,'%H:%i') as time, stream_height"))
                 ->where('sensor_id','=',$sensor_id)
-
-                ->orderBy('timestamp','desc')
-
+                ->groupBy('time')
                 ->limit(24)->get();
 
         }
@@ -66,7 +64,7 @@ class sensorLogChartCtrl extends Controller
    if(!empty($sensortrigger))
         {
             // add row data into datatable for Chart
-            if($graph_type==1) {
+            if($graph_type==2) {
                 for ($i = count($sensorlogs) - 1; $i >= 0; $i--) {
                     $sensorlog = $sensorlogs[$i];
                      $sensenlogTable->addRow([$sensorlog->time, $sensorlog->stream_height, $sensortrigger->level_emergency, $sensortrigger->level_warning]);
