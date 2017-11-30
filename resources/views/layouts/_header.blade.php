703cba48e4153c8f6dde008fb060511bd63ea244
chenda <chenda.loeurt@gmail.com>
2017-11-30 10:31:30 +0700

    change file header because of changing logo margin

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
     <div class="navbar-header">
       <a class="navbar-brand" href="{{ url('/') }}">
         <div class="row" style="background: white;">
-          <div class="col-sm-3 col-md-3 col-lg-3" style="padding-bottom:10px;margin-top:-10px;">
+          <div class="col-sm-3 col-md-3 col-lg-3" style="padding-bottom:10px;margin-top: 3px;">
             <img src="/EWS1294b.gif" width="35%">
           </div>
         <!--   <div class="col-sm-9 col-md-9 col-lg-9" style="text-align: center; font-family: Roboto,Arial,sans-serf; ">          
